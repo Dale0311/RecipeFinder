9b9314871060eaa022c483206d56d24771800069
Dale <Danntabachoyy@gmail.com>
2024-11-08 22:14:25 +0800

    u: for checking useFetch modification

--- src/hooks/useFetch.ts
+++ src/hooks/useFetch.ts
@@ -2,17 +2,19 @@ import { useEffect, useState } from 'react';
 import { TModifiedResponse } from '../types';
 import instance from '../api/config';
 
+type TCurrentPagination = {
+  totalPage: number;
+  currentPage: number;
+  currentURL: string;
+  prevURL: string;
+  nextURL: string;
+};
+
 type TState = {
   data: TModifiedResponse | undefined;
   loading: boolean;
   error: string;
-  history: {
-    totalPage: number;
-    currentPage: number;
-    currentURL: string;
-    prevURL: string;
-    nextURL: string;
-  }[];
+  history: TCurrentPagination[];
 };
 
 const useFetch = () => {
@@ -27,29 +29,61 @@ const useFetch = () => {
     fetchData('Filipino');
   }, []);
 
-  const fetchData = async (q: string) => {
+  // const fetchData = async (q: string) => {
+  const fetchData = async (
+    param: string,
+    query?: {
+      q: string;
+      currentPagination: TCurrentPagination;
+    }
+  ) => {
     setState((p) => ({ ...p, loading: true }));
-    try {
-      const res = await instance.get<TModifiedResponse>('', {
-        params: {
-          q,
-        },
-        // const res = await instance.get<TModifiedResponse>('', {
-        //   params: {
-        //     q,
-        //   },
-      });
+    const forNewQuery: boolean = Boolean(param) && typeof query === 'undefined';
 
-      const totalPage = res.data.count / 20;
-      const currentPage = res.data.to / 20;
-      const nextURL = res.data._links.next.href;
-      const currentURL: string = res.request.responseURL;
-      const prevURL = '';
+    try {
+      const res = await instance.get<TModifiedResponse>(
+        forNewQuery ? '' : query!.q,
+        {
+          params: forNewQuery
+            ? {
+                q: param,
+              }
+            : {},
+          // const res = await instance.get<TModifiedResponse>('', {
+          //   params: {
+          //     q,
+          //   },
+        }
+      );
+
+      // const newPage = {
+      //   totalPage: Math.ceil(res.data.count / 20),
+      //   currentPage: Math.ceil(res.data.to / 20),
+      //   nextURL: res.data._links.next.href,
+      //   currentURL: currentPagination.nextURL, //nextURL would be stored in currentURL on next render
+      //   prevURL: currentPagination.currentURL, //currentURL would be store in previousURL when user click next
+      // };
+      const newPage = {
+        totalPage: res.data.count / 20,
+        currentPage: res.data.to / 20,
+        nextURL: res.data._links.next.href,
+        currentURL: forNewQuery
+          ? (res.request.responseURL as string)
+          : query!.currentPagination.nextURL,
+        prevURL: forNewQuery ? '' : query!.currentPagination.currentURL,
+      };
+
+      const updateHistory = state.history;
+
+      // flawed, because what if i just fetch the next page?
+      forNewQuery || query?.q === newPage.currentURL
+        ? updateHistory.push(newPage)
+        : updateHistory.pop();
 
       setState((p) => ({
         ...p,
         data: res.data,
-        history: [{ totalPage, currentPage, nextURL, currentURL, prevURL }],
+        history: updateHistory,
       }));
     } catch (error) {
       setState((p) => ({ ...p, error: 'Something went wrong' }));
@@ -62,7 +96,6 @@ const useFetch = () => {
   const onPageChange = async (action: 'next' | 'prev') => {
     setState((p) => ({ ...p, loading: true }));
     const currentHistoryIndex = state.history.length - 1;
-    const tempHistory = state.history;
 
     const nextPageClick = Boolean(action === 'next');
 
@@ -73,29 +106,45 @@ const useFetch = () => {
       ? currentPagination.nextURL
       : currentPagination.prevURL;
 
-    try {
-      const res = await instance.get<TModifiedResponse>(q);
-
-      const newPage = {
-        totalPage: Math.ceil(res.data.count / 20),
-        currentPage: Math.ceil(res.data.to / 20),
-        nextURL: res.data._links.next.href,
-        currentURL: currentPagination.nextURL, //nextURL would be stored in currentURL on next render
-        prevURL: currentPagination.currentURL, //currentURL would be store in previousURL when user click next
-      };
-
-      nextPageClick ? tempHistory.push(newPage) : tempHistory.pop();
-      setState((p) => ({
-        ...p,
-        data: res.data,
-        history: tempHistory,
-      }));
-    } catch (error) {
-      setState((p) => ({ ...p, error: 'Something went wrong' }));
-    } finally {
-      setState((p) => ({ ...p, loading: false }));
-    }
+    fetchData('', { q, currentPagination });
   };
+  // const onPageChange = async (action: 'next' | 'prev') => {
+  //   setState((p) => ({ ...p, loading: true }));
+  //   const currentHistoryIndex = state.history.length - 1;
+  //   const tempHistory = state.history;
+
+  //   const nextPageClick = Boolean(action === 'next');
+
+  //   const currentPagination = state.history[currentHistoryIndex];
+
+  //   //que
+  //   const q = nextPageClick
+  //     ? currentPagination.nextURL
+  //     : currentPagination.prevURL;
+
+  //   try {
+  //     const res = await instance.get<TModifiedResponse>(q);
+
+  //     const newPage = {
+  //       totalPage: Math.ceil(res.data.count / 20),
+  //       currentPage: Math.ceil(res.data.to / 20),
+  //       nextURL: res.data._links.next.href,
+  //       currentURL: currentPagination.nextURL, //nextURL would be stored in currentURL on next render
+  //       prevURL: currentPagination.currentURL, //currentURL would be store in previousURL when user click next
+  //     };
+
+  //     nextPageClick ? tempHistory.push(newPage) : tempHistory.pop();
+  //     setState((p) => ({
+  //       ...p,
+  //       data: res.data,
+  //       history: tempHistory,
+  //     }));
+  //   } catch (error) {
+  //     setState((p) => ({ ...p, error: 'Something went wrong' }));
+  //   } finally {
+  //     setState((p) => ({ ...p, loading: false }));
+  //   }
+  // };
 
   return { ...state, fetchData, onPageChange };
 };
